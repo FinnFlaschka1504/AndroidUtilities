8261f80dfb18a75ae9198f9720dd8174d574ebc1
FLAS008 <finngithub1504>
2019-11-27 10:46:11 +0100

    Initial commit

---
+++ androidUtilities/src/main/java/com/finn/androidUtilities/Test.java
@@ -0,0 +1,11 @@
+package com.finn.androidUtilities;
+
+import android.content.Context;
+import android.widget.Toast;
+
+public class Test {
+    
+    public static void test(Context context){
+        Toast.makeText(context, "Test", Toast.LENGTH_SHORT).show();
+    }
+}
